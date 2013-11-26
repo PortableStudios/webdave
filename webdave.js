4402ab0c5f3821feb4416a9591cc090377c9d90d
Jerram Watters <jerramw@gmail.com>
2013-11-26 12:24:00 +1100

    switching to input commands

--- webdave.js
+++ webdave.js
@@ -1,43 +1,46 @@
-//var args = process.argv.slice(2);
-//console.log('Hello ' + args.join(' ') + '!');
-
-if (!String.prototype.format) {
-  String.prototype.format = function() {
-    var args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) {
-      return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-      ;
-    });
-  };
-}
-
 var $ = require('jquery');
 var util = require('util')
 var exec = require('child_process').exec;
-function puts(error, stdout, stderr) { util.puts(stdout) }
-
-
 var local = '/var/vhosts/bigcommerce/blueprint-template/';
-/**
-var files = [
-    'Panels/Footer.html',
-    'Panels/Header.html',
-    'Styles/portableshops-reset.css',
-    'Styles/portableshops.css',
-    'img/sprite-social-small.png'
-];
-*/
-
-var files = process.argv.slice(2);
 var remote = 'https://store-ihsuv3.mybigcommerce.com/dav/template/';
 var user = 'admin';
 var pass='jwatters2013';
 
-$.each(files, function( index, path ){
-    command = util.format("curl --verbose --digest --user '%s:%s' -T '%s%s' '%s%s'", user, pass, local, path, remote, path);
-    console.log(path);
-    exec(command, puts);
-});
+function puts(error, stdout, stderr) {
+    util.print('stdout: ' + stdout);
+    util.print('stderr: ' + stderr);
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    }
+}
 
+function files (){
+    var files = process.argv.slice(2);
+
+    $.each(files, function( index, path ){
+        command = util.format("curl --digest --user '%s:%s' -T '%s%s' '%s%s'", user, pass, local, path, remote, path);
+        console.log(path);
+        result = exec(command, puts);
+    });
+}
+
+function all(){
+    console.log('push all files from a function ');
+
+}
+
+var input = process.argv.slice(2);
+
+
+if (input[0] == 'git') {
+    console.log('Do a git type command');
+
+
+}
+else if (input[0] == 'all') {
+    all();
+}
+else {
+    console.log('try using input as file list');
+    files();
+}
