1e2015d1c25b6ec6f027aec50e4587fe1d4f73cb
Jerram Watters <jerramw@gmail.com>
2013-12-11 10:29:07 +1100

    global, git
    change to run from command line
    move endpoint settings to yml
    add option to upload all staged files

--- webdave.js
+++ webdave.js
@@ -1,6 +1,21 @@
+#!/usr/bin/env node
 /**
  * WebDave - your webdav mate
+ * Add install path
+ * export PATH="$PATH:/home/jerram/node_modules/:/var/www/cakephp/cake/console/"
  *
+ * set up webdave.yml in your working folder like
+ * remote: https://store-abdce1.mybigcommerce.com/dav/template/
+ * user: admin
+ * pass: yourpass
+ *
+ * run the script from your working folder
+ * /var/vhosts/bigcommerce/your-bc-template$ webdave test.html
+ *
+ */
+
+ /**
+ * TODO
  * So I want to enable async uploads, but limited to say ten.
  * I can use the async library, need to add it at line 74
  * https://github.com/caolan/async
@@ -14,10 +29,10 @@
  * http://tools.ietf.org/html/rfc2518#page-26
  *
  * Then add git awareness
+ * https://github.com/pvorb/node-git-wrapper
  *
  * Finally it would be nice to run the script at the start of a session
  * and push whatever is staged
- *
  */
 
 var $ = require('jquery');
@@ -27,17 +42,24 @@ var fs = require('fs');
 var loaddir = require('loaddir');
 var execSync = require("exec-sync");
 var async = require("async");
+var local = process.cwd();
+var YAML = require('yamljs');
+var endpoint = YAML.load('webdave.yml');
+var sys = require('sys')
+var exec = require('child_process').exec;
 
-var local = '/var/vhosts/bigcommerce/blueprint/';
-var remote = 'https://store-ihsuv3.mybigcommerce.com/dav/template/';
-var user = 'admin';
-var pass='jwatters2013';
+// TWS
+//var local = '/var/vhosts/tws/template/';
+//var remote = 'https://store-raa4hjlf.mybigcommerce.com/dav/template/';
+//var user = 'portable';
+//var pass='portshop1!';
 
 var input = process.argv.slice(2);
-var queue = async.queue(uploadFile, 10); // Run ten simultaneous uploads
+var queue = async.queue(runUpload, 2); // Run ten simultaneous uploads
 
 queue.drain = function() {
     console.log("All files are uploaded");
+    //process.exit(code=0);
 };
 
 function puts(error, stdout, stderr) {
@@ -49,7 +71,7 @@ function puts(error, stdout, stderr) {
 }
 
 function all(){
-    console.log('push all files from a function ');
+    console.log('push all files from a location');
     files = []
     loaddir({
         path: local,
@@ -67,19 +89,37 @@ function files (){
 
 function uploadFiles(files, callback){
     console.log('Uploading...');
-    $.each(files, function( index, path ){
-        command = util.format("curl --digest --user '%s:%s' -T '%s%s' '%s%s'", user, pass, local, path, remote, path);
-        console.log(path);
+    $.each(files, function( index, file ){
+        if (file != '') {
+            command = util.format("curl --digest --user '%s:%s' -T '%s/%s' '%s%s'", endpoint.user, endpoint.pass, local, file, endpoint.remote, file);
+            console.log(file);
+            result = execSync(command, puts);
+        }
         // Convert this to async here I think
-        //queue.push(file);
-        result = execSync(command, puts);
+        //queue.push(command, function (err) {
+        //        console.log('queued ' + path);
+        //});
     });
-    callback();
-    process.exit(code=0)
+    process.exit(code=0); // required for sync uploads, otherwise its just waits for more files
 }
+
+function runUpload(command, callback){
+    console.log(command);
+    result = exec(command);
+    callback();
+}
+
+function gitFiles(error, stdout, stderr){
+    console.log('Breaking up git file list and sending to upload');
+    stdout.replace(/[\n]/g, '');
+    var files = stdout.split('\n');
+    uploadFiles(files);
+}
+
 
 if (input[0] == 'git') {
     console.log('Do a git type command');
+    exec("git diff --name-only --cached", gitFiles);
 }
 else if (input[0] == 'all') {
     all();
